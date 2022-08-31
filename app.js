0df12934bba1f7b7ae89bb5261a97837e6870a26
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2022-08-31 17:52:39 -0500

    Add files via upload
    updated JavaScript file

---
+++ app.js
@@ -0,0 +1,16 @@
+const rateStart = document.querySelector(".rating-state");
+const thankYou = document.querySelector(".thank-you");
+const form = document.querySelector(".rating-form");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault(); //prevents Browser from refreshing
+    const selector = "input[name=ratings]:checked";
+    const checkedInput = document.querySelector(selector);
+
+    // User needs to select an Option in order to go on
+    if (checkedInput === null) return alert('Please choose a rating before submitting!')
+    const selectedRating = document.querySelector(".selection");
+    selectedRating.textContent = "You selected " + checkedInput.getAttribute("value") + " out of 5"
+    rateStart.classList.add("hidden");
+    thankYou.classList.remove("hidden");
+})
